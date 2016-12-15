be61618f3717ff49147390e2c91ad124278bc21b
Loz Jackson <lozjackson@talktalk.net>
2016-12-15 19:37:21 +0000

    import jspdf

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-jspdf'
+  name: 'ember-jspdf',
+
+  init: function(name) {
+    this._super.init && this._super.init.apply(this, arguments);
+    var assets_path = require('path').join('jspdf','dist','jspdf.min.js');
+    this.treePaths['vendor'] = require.resolve('jspdf').replace(assets_path, '');
+  },
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+    app.import('vendor/jspdf/dist/jspdf.min.js');
+  }
 };
